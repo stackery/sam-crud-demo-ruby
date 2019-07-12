3c0ebcf7804e7e942c37383aa43c21f0f29f14cf
Anna Spysz <anna.spysz@stackery.io>
2019-07-12 10:34:12 -0700

    Create function

--- src/createUser/function.rb
+++ src/createUser/function.rb
@@ -1,43 +1,40 @@
-require 'aws-sdk'
-require 'json'
+require "aws-sdk"
+require "json"
 
 def handler(event:, context:)
-  event = JSON.generate(event)
+  user = event["body"]
   context = JSON.generate(context.inspect)
 
   dynamodb = Aws::DynamoDB::Client.new
-  table_name = ENV['TABLE_NAME']
-  user = event[:body]
-
-  puts user
+  table_name = ENV["TABLE_NAME"]
 
   params = {
     table_name: table_name,
     item: {
-      'id' => user['id'],
-      'FirstName' => user['firstName'],
-      'LastName' => user['lastName'],
-      'FavoriteColor' => user['color']
+      "id" => user["id"],
+      "FirstName" => user["firstName"],
+      "LastName" => user["lastName"],
+      "FavoriteColor" => user["color"]
     }, 
-    return_consumed_capacity: 'TOTAL',
-    condition_expression: 'attribute_not_exists(id)'
+    return_consumed_capacity: "TOTAL",
+    condition_expression: "attribute_not_exists(id)"
   }
 
   begin
-    # Write a new item to the ItemTable
+    # Add a new user to the table
     dynamodb.put_item(params)
-    item_id = params[:item]['id']
-    puts `Writing item #{item_id} to table #{table_name}.`
+    item_id = params[:item]["id"]
+    puts "Writing item #{item_id} to table #{table_name}."
 
   rescue  Aws::DynamoDB::Errors::ServiceError => error # stop execution if dynamodb is not available
-    puts `Error writing to table #{table_name}. Make sure this function is running in the same environment as the table.`
+    puts "Error writing to table #{table_name}:"
     puts error.message
   end
 
   # Return a 200 response if no errors
   response = {
-    'statusCode': 200,
-    'body': 'Success!'
+    "statusCode": 200,
+    "body": "Success!"
   }
 
   return response
